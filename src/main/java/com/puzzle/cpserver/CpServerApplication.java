26da015fd7d0ac2613488cb838845d835228c9da
nikchernyakov <nikchernyakov@yandex.ru>
2019-06-05 23:57:11 +0300

    Init project

---
+++ src/main/java/com/puzzle/cpserver/CpServerApplication.java
@@ -0,0 +1,13 @@
+package com.puzzle.cpserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CpServerApplication.class, args);
+    }
+
+}
