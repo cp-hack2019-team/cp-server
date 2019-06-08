ee1bd630d825e54903b243ea16cc2ecf8209f092
Ivan Beziazychnyi <ibez@yandex-team.ru>
2019-06-08 18:49:04 +0300

    pass user id after signin

--- src/main/java/com/puzzle/controller/AuthController.java
+++ src/main/java/com/puzzle/controller/AuthController.java
@@ -1,6 +1,5 @@
 package com.puzzle.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -9,11 +8,11 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
-
 import java.util.HashMap;
 import java.util.Map;
 
 
+import com.puzzle.dao.entity.User;
 import com.puzzle.dao.repository.UserRepository;
 import com.puzzle.resource.AuthenticationRequest;
 import com.puzzle.security.jwt.JwtTokenProvider;
@@ -41,13 +40,13 @@ public class AuthController {
         try {
             String username = data.getUsername();
             authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, data.getPassword()));
-            String token = jwtTokenProvider.createToken(username,
-                this.userRepository.findByLogin(username)
-                    .orElseThrow(() -> new UsernameNotFoundException("Username " + username + "not found"))
-                    .getRoles());
+            User user = userRepository.findByLogin(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Username " + username + "not found"));
+            String token = jwtTokenProvider.createToken(username, user.getRoles());
             Map<Object, Object> model = new HashMap<>();
             model.put("username", username);
             model.put("token", token);
+            model.put("id", user.getUuid());
             return ok(model);
         } catch (AuthenticationException e) {
             throw new BadCredentialsException("Invalid username/password supplied");
