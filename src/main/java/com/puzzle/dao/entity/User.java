0becb9e0196f89bb8b092d8137da578801c8ffac
Ivan Beziazychnyi <ibez@yandex-team.ru>
2019-06-09 13:37:39 +0300

    fixed build

--- src/main/java/com/puzzle/dao/entity/User.java
+++ src/main/java/com/puzzle/dao/entity/User.java
@@ -12,7 +12,6 @@ import java.util.List;
 import java.util.Set;
 
 
-import com.sun.xml.internal.ws.developer.UsesJAXBContext;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
